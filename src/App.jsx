21388a0fca5c6befb1ea49059044a833a3f9a421
pszempszemek <jestempszemek@gmail.com>
2023-01-19 13:18:43 +0100

    InitialCommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Component1 from "./components/Component1";
+
+export default function App() {
+
+  return (
+      <main>
+        <h1>hello</h1>
+        <Component1 />
+      </main>
+  );
+}
---
+++ src/components/Component1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Component1(){
+  return (
+  <div>
+    <h2>Component1</h2>
+  </div>
+  )
+}

fa61c404c76c9a071cecb87936a32d2697f137c0
pszempszemek <jestempszemek@gmail.com>
2023-01-19 13:28:19 +0100

    Modify: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import Component1 from "./components/Component1";
+import React, { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
 
+
+  const expensiveCalculation = (num) => {
+    console.log('Calculating');
+    for (let i =0; i<100000000; i++) {
+      num +=1;
+    }
+    return num;
+  };
+  
+  const increment = () => {
+    setCount((c) => c+1);
+  }
+
+  const addTodo = () => {
+    setTodos((t) => [...t,"New Todo"]);
+  }
+  
   return (
       <main>
-        <h1>hello</h1>
-        <Component1 />
+        <div>
+          <h2>My Todos</h2>
+          {todos.map((todo, index) => {
+            return <p key={index}>{todo}</p>;
+          })}
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
+        <hr />
+        <div>
+          Conut: {count}
+          <button onClick={increment}>+</button>
+          <h2>Expensive Calculation</h2>
+          {calculation}
+        </div>
       </main>
   );
 }
